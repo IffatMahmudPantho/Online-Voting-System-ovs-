9479d15845387bfe739afc4d1f1d0dbab826b833
Iffat Mahmud Pantho <mahmud.pantha@northsouth.edu>
2018-12-07 03:08:23 +0600

    Changes in regester.blade.php

--- resources/views/pages/register.blade.php
+++ resources/views/pages/register.blade.php
@@ -2,5 +2,5 @@
 
 @section('content')
 <h1>{{$title}}</h1>
-    <p>If you don't have any account then register here </p>       
+    <p>If you don't have any account then register here!</p>       
 @endsection
