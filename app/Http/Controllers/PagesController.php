e33e0301dde17238ca5625eebdd1078967b4fab9
Iffat Mahmud Pantho <mahmud.pantha@northsouth.edu>
2018-12-07 02:58:02 +0600

    Initial commit home and login

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PagesController extends Controller
+{
+    public function index(){
+        $title = 'Welcome to the Online Voting System';
+        return view('pages.index')->with('title',$title);
+    }
+    public function register(){
+        $title = 'Rgesiter Here';
+        return view('pages.register')->with('title',$title);
+    }
+    public function login(){
+        $data = array(
+            'title'=> 'Login window',
+            //'login'=>['Result of the Previous Election','Create a new Election','Name of the Election : ', 'Set Candidates : ']
+        );
+        return view('pages.login')->with($data);
+    }
+}
---
+++ resources/views/pages/register.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+
+@section('content')
+<h1>{{$title}}</h1>
+    <p>If you don't have any account then register here </p>       
+@endsection
