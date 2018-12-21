1ae48ddecf99896f71969f4f1881fc82e1697d11
Iffat Mahmud Pantho <mahmud.pantha@northsouth.edu>
2018-12-21 22:16:49 +0600

    New navbar created and added to user page

---
+++ resources/views/inc/navbar3.blade.php
@@ -0,0 +1,22 @@
+<nav class="navbar navbar-expand-md navbar-dark bg-dark fixed top">
+
+        <a class="navbar-brand" href="/">Online Voting System</a>
+       
+    
+        </button>
+    
+        <div class="collapse navbar-collapse">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+              <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
+            </li>
+            
+            
+            
+        </ul>
+         
+          
+    
+        </div>
+      </nav>
+    
