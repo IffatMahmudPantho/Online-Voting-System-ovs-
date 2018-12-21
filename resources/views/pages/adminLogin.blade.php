b092384b61f46ac40c177ca5cea5c247b120205d
Mosleh Uddin <m.mosleh27@gmail.com>
2018-12-22 02:03:40 +0600

    update logo for admin login window

--- resources/views/pages/adminLogin.blade.php
+++ resources/views/pages/adminLogin.blade.php
@@ -13,7 +13,7 @@
 
 <body class="text-center">
 <form class="form-signin">
-  <img class="mb-4" src="https://getbootstrap.com/docs/4.0/assets/brand/bootstrap-solid.svg" alt="" width="72" height="72">
+  <img class="mb-4" src="https://png.pngtree.com/svg/20161229/_username_login_1172579.png" alt="" width="72" height="72">
   <h1 class="h4 mb-3 font-weight-normal">if you are an Admin then, <br>Please log in here : </h1>
   <label for="username" class="sr-only">Username</label>
   <input type="username" id="inputUsername" class="form-control" placeholder="Username" required autofocus>
