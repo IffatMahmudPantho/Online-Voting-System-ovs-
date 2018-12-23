a35b29fb214dcc7e79c65b92123392069964b927
Mosleh Uddin <m.mosleh27@gmail.com>
2018-12-23 20:51:09 +0600

    update to show welcome message

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,6 +6,15 @@
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">Dashboard</div>
+                
+                <div class ="row">
+					<div class="col-md-12">
+						<div class="jumbotron">
+							<h2><b>Welcome to 2018 online Voting Portal</b></h2>
+							<h4><i>Helping to Conduct a violent free and fair Voting Experience</i></h4>
+						</div><!--end jumbotron-->    
+					</div><!--end column-->
+				</div><!--end of .row-->
 
                 <div class="card-body">
                     @if (session('status'))
