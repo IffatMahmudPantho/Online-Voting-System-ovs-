808ae3ae324bf398fa8a76abda50aec74ec0acd9
hassanul142 <45876849+hassanul142@users.noreply.github.com>
2018-12-23 19:15:29 +0600

    adding log in connection code

---
+++ resources/views/pages/login.php
@@ -0,0 +1,35 @@
+<?php
+session_start();
+  $Username=$_POST['Username'];
+  $Password=$_POST['Password'];
+  $con=mysqli_connect("localhost","root","","online_voting_system");
+  $query="select * from customer_details where email='$email'";
+  $result=mysqli_query($con,$query);
+  $num=mysqli_num_rows($result);
+  if($num==0){
+    echo "No user with this Username found,Try to <a href='Login.html'>Login</a> again";
+  }
+  else{
+    $row=mysqli_fetch_array($result, MYSQLI_ASSOC);
+    $dbpassword=$row['Password'];
+    $admin="armanshatu@yahoo.com";
+      if($email==$admin){
+        if($Password==$dbpassword){
+      $_SESSION['Email']=$Email;
+      $_SESSION['login']=TRUE;
+      header('location:admin.php');
+        }
+      else{
+      echo "Password doesn't match, Try to <a href='Login.html'>Login</a> again";
+    }
+  }else if($Password==$dbpassword){
+      $_SESSION['Email']=$Email;
+      $_SESSION['login']=TRUE;
+      header('location:user.php');
+    }
+    else{
+      echo "Password doesn't match, Try to <a href='Login.html'>Login</a> again";
+    }
+
+  }
+?>
